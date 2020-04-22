f9f710e3827d882d0ba22680e2de8966694fcabd
Yegor Ruditsa <egorrudica@gmail.com>
2020-04-23 01:17:37 +0300

    Add registration method

---
+++ api/registration.js
@@ -0,0 +1,9 @@
+async ({ login, password }) => {
+  const user = { login, password };
+  const queryResult = application.database
+    .insert('Users', user);
+  if (queryResult === false)
+  {
+    console.log('registration is failed');
+  }
+};
