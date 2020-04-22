ed49551a0c6c7e2b24efb0397074880c23b9bf1a
Yegor Ruditsa <egorrudica@gmail.com>
2020-04-23 00:00:32 +0300

    Add double threading

---
+++ start.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { Worker } = require('worker_threads');
+
+PORTS_NUMBER = 2;
+
+const workers = [];
+for (let i = 0; i < PORTS_NUMBER; ++i)
+{
+  const worker = new Worker('./workerThread.js');
+  workers.push(worker);
+}
+
+process.on('exit', async () => {
+  console.log('Got exit signal');
+  for (let i = 0; i < PORTS_NUMBER; ++i)
+  {
+    worker.postMessage({ name: 'stop' });
+  }
+});
