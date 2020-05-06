a38e99b86d1cb5c9a0eecec50098fb01ccb98c85
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-06 22:49:34 +0300

    Change client api

--- static/client.js
+++ static/client.js
@@ -27,3 +27,10 @@ const loadMethods = methods => {
   }
   return api;
 };
+
+
+
+const api = buildAPI([
+  'registration',
+  'signIn',
+]);
---
+++ storage/delta.sql
@@ -0,0 +1,4 @@
+INSERT INTO Cats (WoolColor, EyeColor, Coloration, TailLength, WoolLength) VALUES
+(0.3, 0.45, 0.1, 0.8, 0.12);
+INSERT INTO Cats (WoolColor, EyeColor, Coloration, TailLength, WoolLength) VALUES
+(0.3, 0.45, 0.1, 0.8, 0.12);
