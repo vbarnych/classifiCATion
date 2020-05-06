c0563d4f9099bde8a29a5cce2ee8183cf1349de0
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-06 20:32:58 +0300

    Add exception handler on status code

--- static/client.js
+++ static/client.js
@@ -6,8 +6,7 @@ const loadMethods = methods => {
   const api = {};
   for (const method of methods)
   {
-  api[method] = (args = {}) =>
-  new Promise((resolve, reject) => {
+  api[method] = (args = {}) =>  new Promise((resolve, reject) => {
     const url = BASE + method;
     console.log(url, args);
     fetch(url, {
@@ -18,6 +17,10 @@ const loadMethods = methods => {
       body: JSON.stringify(args),
     }).then(res => {
       const { status } = res;
+      if (status !== 200) {
+        reject(new Error(`Status Code: ${status}`));
+        return;
+      }
       resolve(res.json());
       });
     });
