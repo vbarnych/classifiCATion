923c009dcdbf9f6417cac06f4a45841ddd514b5e
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-07 12:04:10 +0300

    Add debug logging

--- api/signIn.js
+++ api/signIn.js
@@ -1,10 +1,8 @@
 async ({ login, password }) => {
   const where = { login, password };
-  //const id = await app.database.select('SystemUsers', ['Id'], where);
   const [user] = await app.database.select(
-    'SystemUser', ['Password'], { login }
+    'SystemUsers', ['Password'], { login }
   );
-    //console.log(id);
   if (!id) throw new Error('Incorrect login or password');
   console.log(`Logged user: ${login}`);
   return { result: 'success', userId: user.id };
