dd4d86c8303323112371606bc827d674ad161998
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-11 21:37:05 +0300

    Refactor application configuration

--- mainThread.js
+++ mainThread.js
@@ -1,17 +1,36 @@
 'use strict';
 
-const { Worker } = require('worker_threads');
+//const { Worker } = require('worker_threads');
 
 const PORTS_NUMBER = 2;
 
-const workers = [];
-for (let i = 0; i < PORTS_NUMBER; ++i)
-{
-  const worker = new Worker('./workerThread.js');
-  workers.push(worker);
-}
+//const workers = [];
+const { Worker } = require('worker_threads');
+const path = require('path');
 
-process.on('SIGINT', async () => {
+const Config = require('./lib/config.js');
+
+const PATH = process.cwd();
+const CFG_PATH = path.join(PATH, 'config');
+
+(async () => {
+  const config = await new Config(CFG_PATH);
+  const { units } = config;
+  const count = PORTS_NUMBER;
+  const workers = new Array(count);
+
+  const start = id => {
+    const worker = new Worker('./workerThread.js');
+    workers[id] = worker;
+    worker.on('exit', code => {
+      if (code !== 0) start(id);
+    });
+  };
+
+  for (let id = 0; id < count; id++) start(id);
+
+
+  process.on('SIGINT', async () => {
   console.log('Got exit signal');
   for (let i = 0; i < PORTS_NUMBER; ++i)
   {
@@ -19,7 +38,4 @@ process.on('SIGINT', async () => {
   }
 });
 
-process.on('uncaughtException', error => {
-  console.log('Unhandled exception: ');
-  console.dir(error);
-});
+})();
