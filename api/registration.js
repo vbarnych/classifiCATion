9658e3c77b6822ca96c05e47adffc08120bf55e3
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-13 14:25:26 +0300

    Add password hashing in registr menthod

--- api/registration.js
+++ api/registration.js
@@ -1,11 +1,7 @@
 'use strict';
 
 module.exports = async ({ login, password, fullname }) => {
-  const user = { login, password, fullname };
-  const queryResult = application.database
-    .insert('Users', user);
-  if (queryResult === false)
-  {
-    console.log('registration is failed');
-  }
+  const hash = await app.Crypto.hashPassword(password);
+  await app.sessions.registration(login, hash, fullname);
+  return { result: 'success' };
 };
--- api/signIn.js
+++ api/signIn.js
@@ -1,7 +1,7 @@
 async ({ login, password }) => {
   const user = await app.sessions.getUser(login);
   const hash = user ? user.password : undefined;
-  const correctPass = await application.security.checkPassword(password, hash);
+  const correctPass = await app.security.checkPassword(password, hash);
   if (!user || !correctPass) throw new Error('Authentication failed.');
   console.log(`Logged user: ${login}`);
   return { result: 'success', userId: user.id, fullname: user.fullname, };
