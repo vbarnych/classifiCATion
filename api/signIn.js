6c3f951c5a6560ba2b2612dd93f4aa99c3c9d6ea
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-13 14:07:41 +0300

    Add password checking validation

--- api/signIn.js
+++ api/signIn.js
@@ -1,8 +1,8 @@
 async ({ login, password }) => {
   const user = await app.sessions.getUser(login);
   const hash = user ? user.password : undefined;
-  //const verified = await application.security.validatePassword(password, hash);
-  if (!user) throw new Error('Incorrect login or password');
+  const correctPass = await application.security.validatePassword(password, hash);
+  if (!user || !correctPass) throw new Error('Authentication failed.');
   console.log(`Logged user: ${login}`);
   return { result: 'success', userId: user.id, fullname: user.fullname, };
 };
