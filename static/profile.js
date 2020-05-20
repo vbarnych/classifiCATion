8269e16977a1fa27dec690f6e68b91571582fafd
Yegor Ruditsa <egorrudica@gmail.com>
2020-05-20 18:38:31 +0300

    Add pictures of cats

---
+++ static/profile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const BASE = '/api/';
+
+const loadMethods = methods => {
+  const api = {};
+  for (const method of methods)
+  {
+  api[method] = (args = {}) =>  new Promise((resolve, reject) => {
+    const url = BASE + method;
+    console.log(url, args);
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(args),
+    }).then(res => {
+      const { status } = res;
+      if (status !== 200) {
+        reject(new Error(`Status Code: ${status}`));
+        return;
+      }
+      resolve(res.json());
+      });
+    });
+  }
+  return api;
+};
+
+const api = loadMethods([
+  'signIn',
+  'getFullname',
+]);
+
+const getRandom = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+};
+
+const getCats = async () => {
+  const id = await api.signIn();
+  console.dir(id);
+  const output1 = document.getElementById('output1');
+  output1.innerHTML = 'HTTP POST /api/signIn<br>' + JSON.stringify(id);
+}
+
+login();
--- storage/load.sql
+++ storage/load.sql
@@ -1,2 +1,3 @@
 CREATE USER fresco WITH PASSWORD 'fresco';
 CREATE DATABASE classification OWNER fresco;
+psql -U fresco -d classification -a -f cats.sql
