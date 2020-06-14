1e539d0c0d1d7a8b80d04b62f2d1fe964c905f46
Yegor Ruditsa <egorrudica@gmail.com>
2020-06-14 20:11:20 +0300

    Add cat photo to catpage

--- static/catpage.js
+++ static/catpage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const BASE = '/api/';
+const AWS = 'https://catsstorage.s3.eu-central-1.amazonaws.com/'
+
 const loadMethods = methods => {
   const api = {};
   for (const method of methods)
@@ -37,27 +39,30 @@ const api = loadMethods([
     let fluffy = "SHORT";
     let tail = "SHORT";
     let typeColor = "ONE COLOR";
-    let src = "C:\\Users\\User\\Desktop\\pasha.jpg";
-    console.log((window.location.search).substring(1));
-    const coco = document.cookie;
-    console.log(coco);
 
-    const scenario = async () => {
-      const id = await api.saveGrade({catId: 3, grade: 1});
-      console.log(id);
-    }
+const getPhoto = () => {
+  const catId = Number((window.location.search).substring(7));
+  console.log(catId);
+  let src = AWS + `${catId}.jpg`;
+}
 
-    scenario();
+const scenario = async () => {
+  const id = await api.saveGrade({catId: 3, grade: 1});
+  console.log(id);
+}
+
+getPhoto();
+scenario();
 
 
 
-   document.getElementById("content").children[0].children[1].innerHTML = `<p style='margin-left: 15px;'>${color}</p>`
-   document.getElementById("content").children[1].children[1].innerHTML = `<p style='margin-left: 15px;'>${eyeColor}</p>`
-   document.getElementById("content").children[2].children[1].innerHTML = `<p style='margin-left: 15px;'>${fluffy}</p>`
-   document.getElementById("content").children[3].children[1].innerHTML = `<p style='margin-left: 15px;'>${tail}</p>`
-   document.getElementById("content").children[4].children[1].innerHTML = `<p style='margin-left: 15px;'>${typeColor}</p>`
+document.getElementById("content").children[0].children[1].innerHTML = `<p style='margin-left: 15px;'>${color}</p>`
+document.getElementById("content").children[1].children[1].innerHTML = `<p style='margin-left: 15px;'>${eyeColor}</p>`
+document.getElementById("content").children[2].children[1].innerHTML = `<p style='margin-left: 15px;'>${fluffy}</p>`
+document.getElementById("content").children[3].children[1].innerHTML = `<p style='margin-left: 15px;'>${tail}</p>`
+document.getElementById("content").children[4].children[1].innerHTML = `<p style='margin-left: 15px;'>${typeColor}</p>`
 
-   document.getElementById("content").children[5].innerHTML = `<div><img src=${src} height="200px"></div>`
+document.getElementById("content").children[5].innerHTML = `<div><img src=${src} height="200px"></div>`
 
 
   function getCurrentMark(){
